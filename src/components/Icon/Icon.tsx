381087a9cd5129de13cb8ba949ac6028470c4df3
robscarvalho8 <robscarvalho8@gmail.com>
2023-01-06 19:01:27 -0300

    (feature): add component Icon

---
+++ src/components/Icon/Icon.tsx
@@ -0,0 +1,21 @@
+import { useEffect, useState } from 'react';
+import { height } from 'styled-system';
+
+type IconProps = {
+    variant: string;
+};
+
+export const Icon: React.FC<IconProps> = ({ variant }) => {
+    const [iconSrc, setIconSrc] = useState();
+
+    useEffect(() => {
+        const mount = async () => {
+            const result = await import(`../../../public/${variant}.svg`);
+            if (result) {
+                setIconSrc(result.default);
+            }
+        };
+        mount();
+    }, [variant]);
+    return <img src={iconSrc} width="32px" height="32px" alt={variant} />;
+};
