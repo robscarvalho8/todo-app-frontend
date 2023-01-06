e2e60bb94f936b4967bfef87a310f97558465f8a
robscarvalho8 <robscarvalho8@gmail.com>
2023-01-06 20:24:31 -0300

    (feature): alter method getAll to async

--- src/services/TodoService.ts
+++ src/services/TodoService.ts
@@ -1,7 +1,7 @@
 import { ITodo, ITodoAll } from '../interfaces/ITodo';
 import { Api } from '../providers';
 
-const getAll = () => Api.get<ITodoAll>('/v1/todos');
+const getAll = async () => Api.get<ITodoAll>('/v1/todos');
 
 export const TodoService = {
     getAll,
