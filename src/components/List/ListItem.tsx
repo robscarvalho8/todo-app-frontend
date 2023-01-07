82feef24f4bb86e289b11b16bcc13a5775d0ed7e
robscarvalho8 <robscarvalho8@gmail.com>
2023-01-06 21:17:18 -0300

    (feature): Adapt component List to get allTodo

--- src/components/List/ListItem.tsx
+++ src/components/List/ListItem.tsx
@@ -1,14 +1,28 @@
 import { Column } from '../Column';
 import { Text } from '../Text';
 
+// export type ListItemProps = {
+//     label: string;
+// };
+
+// export const ListItem: React.FC<ListItemProps> = ({ label }) => {
+//     return (
+//         <Column width="100%" bg="rgba(0,0,0,0.2)" p="20px" mb="10px" borderRadius="4px" borderLeft="5px solid #fff">
+//             <Text>{label}</Text>
+//         </Column>
+//     );
+// };
+
 export type ListItemProps = {
-    label: string;
+    id: string;
+    task: string;
+    isDone: number;
 };
 
-export const ListItem: React.FC<ListItemProps> = ({ label }) => {
+export const ListItem: React.FC<ListItemProps> = ({ id, task, isDone }) => {
     return (
         <Column width="100%" bg="rgba(0,0,0,0.2)" p="20px" mb="10px" borderRadius="4px" borderLeft="5px solid #fff">
-            <Text>{label}</Text>
+            <Text>{task}</Text>
         </Column>
     );
 };
--- src/hooks/useTodo.tsx
+++ src/hooks/useTodo.tsx
@@ -1,19 +1,20 @@
 import { useCallback, useState } from 'react';
-import { ITodo, ITodoAll } from '../interfaces/ITodo';
+import { ITodo } from '../interfaces/ITodo';
 import { TodoService } from '../services/TodoService';
 
 export const useTodo = () => {
-    const [tasks, setTasks] = useState<ITodo[]>();
+    const [tasks, setTasks] = useState<ITodo[]>([]);
 
     const getAll = useCallback(async () => {
         const { status, data } = await TodoService.getAll();
 
         if (status !== 200) throw new Error();
-
         setTasks(data.items);
+        console.log(tasks);
     }, []);
 
     return {
+        tasks,
         getAll,
     };
 };
