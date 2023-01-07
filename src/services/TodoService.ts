dbd11707824dcba9392e7dabb12cebc2013d24ac
robscarvalho8 <robscarvalho8@gmail.com>
2023-01-06 22:10:55 -0300

    (feature): create hook useTodo to update a TodoTask

--- src/services/TodoService.ts
+++ src/services/TodoService.ts
@@ -5,7 +5,10 @@ const getAllTodos = () => Api.get<ITodoAll>('/v1/todos');
 
 const createTodo = (todo: Pick<ITodo, 'task' | 'isDone'>) => Api.post<ITodoAll>('/v1/todos', todo);
 
+const updateTodo = (id: string, todo: Pick<ITodo, 'task' | 'isDone'>) => Api.put<ITodoAll>(`/v1/todos/${id}`, todo);
+
 export const TodoService = {
     getAllTodos,
     createTodo,
+    updateTodo,
 };
